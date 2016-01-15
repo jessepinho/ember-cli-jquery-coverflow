978b3b98378ff5d68070f3004b805566a98c8027
Jesse Pinho <jesse@jessepinho.com>
2016-01-15 08:20:03 -0600

    Add coverflow dependency and blueprint

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-jquery-coverflow'
+  name: 'ember-cli-jquery-coverflow',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.js');
+    app.import(app.bowerDirectory + '/coverflow/jquery.coverflow.js');
+    app.import(app.bowerDirectory + '/coverflow/jquery.interpolate.js');
+    app.import(app.bowerDirectory + '/coverflow/jquery.touchSwipe.min.js');
+  }
 };
