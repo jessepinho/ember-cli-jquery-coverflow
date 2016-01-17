20151da78d07111c233bbaffd7bf664fde4fd2b2
Jesse Pinho <jesse@jessepinho.com>
2016-01-17 17:25:34 -0600

    Add more config options and redesign their layout

--- app/controllers/demo.js
+++ app/controllers/demo.js
@@ -1,6 +1,27 @@
 /* global componentHandler */
 import Ember from 'ember';
 
+var optionNames = ['animateStep',
+                   'animateComplete',
+                   'change',
+                   'confirm',
+                   'density',
+                   'duration',
+                   'easing',
+                   'enableClick',
+                   'enableKeyboard',
+                   'enableWheel',
+                   'index',
+                   'innerAngle',
+                   'innerCss',
+                   'innerOffset',
+                   'innerScale',
+                   'outerAngle',
+                   'outerCss',
+                   'outerOffset',
+                   'outerScale',
+                   'select'];
+
 export default Ember.Controller.extend({
   numberOfCovers: 25,
 
@@ -19,12 +40,17 @@ export default Ember.Controller.extend({
   }),
 
   // Properties bound to coverflow:
-  enableWheel: true,
+  density: 1,
+  enableClick: true,
+  enableKeyboard: true,
+  enableWheel: false,
+  index: 0,
   innerAngle: 0,
   innerScale: 0.95,
   outerAngle: -30,
   outerScale: 0.1,
-  observeForRefresh: Ember.computed('numberOfCovers', function() {
+
+  observeForRefresh: Ember.computed(`numberOfCovers,${optionNames.join(',')}`, function() {
     return Date.now();
   })
 });
